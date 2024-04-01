39e0279c5991335c0c2299cfc55bf9c0ef6b8451
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-04-01 17:53:35 +0700

    memasukkan struct mahasiswa

--- struck/struck.cpp
+++ struck/struck.cpp
@@ -1,34 +1,10 @@
 #include <iostream>
-#include <string>
 using namespace std;
-// struct adalah kumpulan kumpulan dari beberapa
-// variabel yang memiliki bnayak tipe
 
-struct Brand {
-	string namaBrand;
-	int tahun;
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
 };
 
-struct Handphone
-{
-	Brand brand;
-	string type;
-	int harga;
-};
-
-int main()
-{
-	Brand merek;
-	merek.namaBrand = "apple";
-	merek.tahun = 1976;
-
-	Handphone hp;
-
-	hp.brand = merek;
-	hp.type = "iphone 7";
-	hp.harga = 1000000;
-
-	cout << "Nama Brand = " << hp.brand.namaBrand << endl;
-
-	
-}
