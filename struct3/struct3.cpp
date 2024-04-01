0ebd89453cc5efb3bc3479ed3122260e2883c508
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-04-01 18:09:32 +0700

    input data

--- struct3/struct3.cpp
+++ struct3/struct3.cpp
@@ -20,5 +20,21 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
 
+	int main() {
+		mahasiswa mhs;
+		cout << "Nomor mahasiswa : ";
+		getline(cin, mhs.nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs.nama);
+
+		cout << "alamat mahasiswa : " << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs.alamat.desa;
+		cout << "\t Nama kota : ";
+		cin >> mhs.alamat.kota;
+		cout << "Umur mahasiswa : ";
+		cin >> mhs.umur;
+
 	
+	}
 }
