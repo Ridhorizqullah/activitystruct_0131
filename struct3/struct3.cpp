9e035af63fae4332e9abce6fa2003d8454628fef
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-04-01 18:09:08 +0700

    getline

--- struct3/struct3.cpp
+++ struct3/struct3.cpp
@@ -12,3 +12,13 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+	
+}
